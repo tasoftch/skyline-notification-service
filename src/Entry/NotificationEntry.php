99e455cc5bf5818807ccc26e0d6f761c3888e16e
Thomas Abplanalp <info@tasoft.ch>
2020-01-10 17:56:40 +0100

    FIX: names

---
+++ src/Entry/NotificationEntry.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2020, TASoft Applications
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace Skyline\Notification\Entry;
+
+use DateTime;
+use Skyline\Notification\Domain\Domain;
+use TASoft\Util\AbstractPDOResource;
+
+class NotificationEntry extends AbstractPDOResource implements NotificationEntryInterface
+{
+    /** @var Domain */
+    private $domain;
+    /** @var string */
+    private $message;
+    /** @var DateTime */
+    private $updated;
+    /** @var string[] */
+    private $tags;
+
+    /**
+     * Entry constructor.
+     * @param Domain $domain
+     * @param string $message
+     * @param DateTime $updated
+     * @param string[] $tags
+     * @param int $id
+     */
+    public function __construct(Domain $domain, string $message, DateTime $updated, array $tags, int $id = -1)
+    {
+        parent::__construct($id);
+
+        $this->domain = $domain;
+        $this->message = $message;
+        $this->updated = $updated;
+        $this->tags = $tags;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getUpdated(): DateTime
+    {
+        return $this->updated;
+    }
+
+    /**
+     * @return Domain
+     */
+    public function getDomain(): Domain
+    {
+        return $this->domain;
+    }
+}
